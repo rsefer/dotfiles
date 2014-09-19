8e1953e9cf2a9b2a389a00b64116eef00e468b82
Robert Sefer <rsefer@gmail.com>
2014-09-19 12:19:39 -0500

    Added mjolnir config
    Need to change mainscreen() when fixed in mjolnir master

---
+++ .mjolnir/init.lua
@@ -0,0 +1,119 @@
+local application = require 'mjolnir.application'
+local hotkey = require 'mjolnir.hotkey'
+local window = require 'mjolnir.window'
+local screen = require 'mjolnir.screen'
+local fnutils = require 'mjolnir.fnutils'
+
+-- Size Left Half
+hotkey.bind({'cmd', 'alt', 'ctrl'}, 'L', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x
+  f.w = sc.w / 2
+  f.y = sc.y
+  f.h = sc.h
+  win:setframe(f)
+end)
+
+-- Size Right Half
+hotkey.bind({'cmd', 'alt', 'ctrl'}, 'R', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.w / 2
+  f.w = sc.w / 2
+  f.y = sc.y
+  f.h = sc.h
+  win:setframe(f)
+end)
+
+-- Size Full
+hotkey.bind({'cmd', 'alt', 'ctrl'}, 'F', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x
+  f.w = sc.w
+  f.y = sc.y
+  f.h = sc.h
+  win:setframe(f)
+end)
+
+-- Size Center
+hotkey.bind({'cmd', 'alt', 'ctrl'}, 'C', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x + (sc.w * 0.15)
+  f.w = sc.w * 0.7
+  f.y = sc.y + (sc.h * 0.15)
+  f.h = sc.h * 0.7
+  win:setframe(f)
+end)
+
+-- Size Left 7/10
+hotkey.bind({'cmd', 'alt', 'ctrl'}, 'N', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x
+  f.w = sc.w * 0.7
+  f.y = sc.y
+  f.h = sc.h
+  win:setframe(f)
+end)
+
+-- Size Right 3/10
+hotkey.bind({'cmd', 'alt', 'ctrl'}, 'M', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x + (sc.w * 0.7)
+  f.w = sc.w * 0.3
+  f.y = sc.y
+  f.h = sc.h
+  win:setframe(f)
+end)
+
+-- Size Right Top 3/10
+hotkey.bind({'cmd', 'alt', 'ctrl'}, ',', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x + (sc.w * 0.7)
+  f.w = sc.w * 0.3
+  f.y = sc.y
+  f.h = sc.h * 0.6
+  win:setframe(f)
+end)
+
+-- Size Right Bottom 3/10
+hotkey.bind({'cmd', 'alt', 'ctrl'}, '.', function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x + (sc.w * 0.7)
+  f.w = sc.w * 0.3
+  f.y = sc.y + (sc.h * 0.6)
+  f.h = sc.h * 0.4
+  win:setframe(f)
+end)
+
+-- Move Left Edge
+hotkey.bind({'cmd', 'alt', 'ctrl'}, ";", function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.x
+  win:setframe(f)
+end)
+
+-- Move Right Edge
+hotkey.bind({'cmd', 'alt', 'ctrl'}, "'", function()
+  local win = window.focusedwindow()
+  local sc = screen.mainscreen():fullframe()
+  local f = win:frame()
+  f.x = sc.w - f.w
+  win:setframe(f)
+end)
