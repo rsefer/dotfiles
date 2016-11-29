6320c0c792ca2b24a29cb8ef14fdb623456ca766
Robert Sefer <rsefer@gmail.com>
2016-11-29 16:18:45 -0600

    Simplified grid

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -29,162 +29,79 @@ end
 
 hs.window.animationDuration = 0
 hs.window.setFrameCorrectness = true
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
+hs.grid.GRIDWIDTH = 100
+hs.grid.GRIDHEIGHT = 100
+
+local gridset = function(x, y, w, h)
+  return function()
+    local win = hs.window.focusedWindow()
+    hs.grid.set(
+      win,
+      { x = x, y = y, w = w, h = h },
+      win:screen()
+    )
+  end
+end
+
+local windowGridKeyCombo = {'cmd', 'alt', 'ctrl'}
 
 -- Size Left Half
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'L', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x
-  f.w = max.w / 2
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
-end)
-
+hs.hotkey.bind(windowGridKeyCombo, 'L', gridset(0, 0, 50, 100))
 -- Size Right Half
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'R', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + max.w / 2
-  f.w = max.w / 2
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
-end)
-
+hs.hotkey.bind(windowGridKeyCombo, 'R', gridset(50, 0, 50, 100))
 -- Size Full
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'F', function()
+hs.hotkey.bind(windowGridKeyCombo, 'F', gridset(0, 0, 100, 100))
+-- Size Centered
+hs.hotkey.bind(windowGridKeyCombo, 'C', gridset(15, 15, 77.5, 70))
+-- Size Left 2/3-ish
+hs.hotkey.bind(windowGridKeyCombo, 'N', gridset(0, 0, 77.5, 100))
+-- Size 2/3-ish Centered
+hs.hotkey.bind(windowGridKeyCombo, 'X', gridset(11.25, 0, 77.5, 100))
+-- Size Right 1/3-ish
+hs.hotkey.bind(windowGridKeyCombo, 'M', gridset(77.5, 0, 30, 100))
+-- Size Right 1/3-ish Top 1/2-ish
+hs.hotkey.bind(windowGridKeyCombo, ',', gridset(77.5, 0, 30, 55))
+-- Size Right 1/3-ish Bottom 1/2-ish
+hs.hotkey.bind(windowGridKeyCombo, '.', gridset(77.5, 60, 30, 40))
+-- Move to Top Edge
+hs.hotkey.bind(windowGridKeyCombo, 'T', function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x
-  f.w = max.w
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
+  local currentRect = hs.grid.get(win)
+  hs.grid.set(
+    win,
+    { x = currentRect.x, y = 0, w = currentRect.w, h = currentRect.h },
+    win:screen()
+  )
 end)
-
--- Size Center
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'C', function()
+-- Move to Bottom Edge
+hs.hotkey.bind(windowGridKeyCombo, 'B', function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + (max.w * 0.15)
-  f.w = max.w * 0.7
-  f.y = max.y + (max.h * 0.15)
-  f.h = max.h * 0.7
-  win:setFrame(f)
+  local currentRect = hs.grid.get(win)
+  hs.grid.set(
+    win,
+    { x = currentRect.x, y = (100 - currentRect.h), w = currentRect.w, h = currentRect.h },
+    win:screen()
+  )
 end)
-
--- Size Left 7.75/10
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'N', function()
+-- Move to Left Edge
+hs.hotkey.bind(windowGridKeyCombo, ';', function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x
-  f.w = max.w * 0.775
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
+  local currentRect = hs.grid.get(win)
+  hs.grid.set(
+    win,
+    { x = 0, y = currentRect.y, w = currentRect.w, h = currentRect.h },
+    win:screen()
+  )
 end)
-
--- Size Left 7.75/10 (offset 2.25)
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'X', function()
+-- Move to Right Edge
+hs.hotkey.bind(windowGridKeyCombo, "'", function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + (max.w * 0.225)
-  f.w = max.w * 0.550
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
-end)
-
--- Size Right 3/10
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'M', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + (max.w * 0.775)
-  f.w = max.w * 0.225
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
-end)
-
--- Size Right Top 3/10
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, ',', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + (max.w * 0.775)
-  f.w = max.w * 0.225
-  f.y = max.y
-  f.h = max.h * 0.55
-  win:setFrame(f)
-end)
-
--- Size Right Bottom 3/10
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, '.', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + (max.w * 0.775)
-  f.w = max.w * 0.225
-  f.y = max.y + (max.h * 0.6)
-  f.h = max.h * 0.4
-  win:setFrame(f)
-end)
-
--- Size Top Half
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'T', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.y = 0
-  f.h = max.h / 2
-  win:setFrame(f)
-end)
-
--- Size Bottom Half
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'B', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.y = max.h * 0.52
-  f.h = max.h * 0.50
-  win:setFrame(f)
-end)
-
--- Move Left Edge
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, ';', function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x
-  win:setFrame(f)
-end)
-
--- Move Right Edge
-hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, "'", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + max.w - f.w
-  win:setFrame(f)
+  local currentRect = hs.grid.get(win)
+  hs.grid.set(
+    win,
+    { x = (100 - currentRect.w), y = currentRect.y, w = currentRect.w, h = currentRect.h },
+    win:screen()
+  )
 end)
