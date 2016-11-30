f049ca60f23d239cfb24df16c1142137e91a5d9a
Robert Sefer <rsefer@gmail.com>
2016-11-30 16:34:54 -0600

    Window size adjustments

--- .hammerspoon/windows.lua
+++ .hammerspoon/windows.lua
@@ -26,34 +26,34 @@ hs.hotkey.bind(windowGridKeyCombo, 'R', gridset(50, 0, 50, 100))
 -- Size Full
 hs.hotkey.bind(windowGridKeyCombo, 'F', gridset(0, 0, 100, 100))
 -- Size Centered
-hs.hotkey.bind(windowGridKeyCombo, 'C', gridset(15, 15, 77.5, 70))
--- Size Left 2/3-ish
-hs.hotkey.bind(windowGridKeyCombo, 'N', gridset(0, 0, 77.5, 100))
--- Size 2/3-ish Centered
-hs.hotkey.bind(windowGridKeyCombo, 'X', gridset(11.25, 0, 77.5, 100))
--- Size Right 1/3-ish
-hs.hotkey.bind(windowGridKeyCombo, 'M', gridset(77.5, 0, 30, 100))
--- Size Right 1/3-ish Top 1/2-ish
-hs.hotkey.bind(windowGridKeyCombo, ',', gridset(77.5, 0, 30, 55))
--- Size Right 1/3-ish Bottom 1/2-ish
-hs.hotkey.bind(windowGridKeyCombo, '.', gridset(77.5, 60, 30, 40))
--- Move to Top Edge
+hs.hotkey.bind(windowGridKeyCombo, 'C', gridset(12.5, 12.5, 75, 75))
+-- Size Left 3/4ths
+hs.hotkey.bind(windowGridKeyCombo, 'N', gridset(0, 0, 75, 100))
+-- Size 3/4ths Centered
+hs.hotkey.bind(windowGridKeyCombo, 'X', gridset(12.5, 0, 75, 100))
+-- Size Right 1/4th
+hs.hotkey.bind(windowGridKeyCombo, 'M', gridset(75, 0, 25, 100))
+-- Size Right 1/4th Top 1/2-ish
+hs.hotkey.bind(windowGridKeyCombo, ',', gridset(75, 0, 25, 55))
+-- Size Right 1/4th Bottom 1/2-ish
+hs.hotkey.bind(windowGridKeyCombo, '.', gridset(75, 60, 25, 40))
+-- Size Half Height, Top Edge
 hs.hotkey.bind(windowGridKeyCombo, 'T', function()
   local win = hs.window.focusedWindow()
   local currentRect = hs.grid.get(win)
   hs.grid.set(
     win,
-    { x = currentRect.x, y = 0, w = currentRect.w, h = currentRect.h },
+    { x = currentRect.x, y = 0, w = currentRect.w, h = 50 },
     win:screen()
   )
 end)
--- Move to Bottom Edge
+-- Size Half Height, Bottom Edge
 hs.hotkey.bind(windowGridKeyCombo, 'B', function()
   local win = hs.window.focusedWindow()
   local currentRect = hs.grid.get(win)
   hs.grid.set(
     win,
-    { x = currentRect.x, y = (100 - currentRect.h), w = currentRect.w, h = currentRect.h },
+    { x = currentRect.x, y = 50, w = currentRect.w, h = 50 },
     win:screen()
   )
 end)
