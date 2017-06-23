27c206893d4864b77a0f955636c11d2600bf29ab
Robert Sefer <rsefer@gmail.com>
2017-06-23 11:49:15 -0500

    Icon play/pause indication

--- .hammerspoon/overcast.lua
+++ .hammerspoon/overcast.lua
@@ -2,6 +2,9 @@
 local overcastWebview = nil
 local isShown = false
 local iconSize = 14.0
+local updateStatusTimer = nil
+local iconOrange = hs.image.imageFromPath('images/overcast_orange.pdf'):setSize({ w = iconSize, h = iconSize })
+local iconBlack = hs.image.imageFromPath('images/overcast_black.pdf'):setSize({ w = iconSize, h = iconSize })
 
 function toggleWebview()
   if isShown then
@@ -15,8 +18,7 @@ end
 
 local overcastMenu = hs.menubar.new()
 overcastMenu:setClickCallback(toggleWebview)
-local icon = hs.image.imageFromPath('images/overcast_orange.pdf')
-overcastMenu:setIcon(icon:setSize({ w = iconSize, h = iconSize }), false)
+overcastMenu:setIcon(iconBlack, false)
 local overcastMenuFrame = overcastMenu:frame()
 
 local screen = hs.window.focusedWindow():screen()
@@ -25,9 +27,16 @@ local margin = 100
 local viewWidth = 350
 local viewHeight = 400
 local rect = hs.geometry.rect((overcastMenuFrame.x + overcastMenuFrame.w / 2) - (viewWidth / 2), overcastMenuFrame.y, viewWidth, viewHeight)
-local js = hs.webview.usercontent.new(hs.host.uuid())
-localjsScript = "$('.navlink:eq(1), .fullart_container, #speedcontrols').css('display', 'none'); $('h2.ocseparatorbar:first()').css('margin-top', '0px');"
-js:injectScript({ source = localjsScript, mainFrame = true, injectionTime = 'documentEnd' })
+local name = 'id' .. hs.host.uuid():gsub('-', '')
+local js = hs.webview.usercontent.new(name)
+localjsScript = "$('.navlink:eq(1), .fullart_container, #speedcontrols').css('display', 'none'); $('h2.ocseparatorbar:first()').css('margin-top', '0px');" .. "setInterval(function() { webkit.messageHandlers." .. name .. ".postMessage({ isPlaying: !$('audio').prop('paused') }); }, 3000);"
+js:injectScript({ source = localjsScript, mainFrame = true, injectionTime = 'documentEnd' }):setCallback(function(message)
+  if message.body.isPlaying then
+    overcastMenu:setIcon(iconOrange, false)
+  else
+    overcastMenu:setIcon(iconBlack, false)
+  end
+end)
 
 overcastWebview = hs.webview.new(rect, { developerExtrasEnabled = true }, js)
   :url('https://overcast.fm/podcasts')
