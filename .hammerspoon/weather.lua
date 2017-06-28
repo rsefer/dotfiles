75fff9fff7a8a123b9fb7f10b2c84cbccf81f501
Robert Sefer <rsefer@gmail.com>
2017-06-28 10:32:30 -0500

    Added verbose weather, DRYed

--- .hammerspoon/weather.lua
+++ .hammerspoon/weather.lua
@@ -19,37 +19,61 @@ if file_exists('darksky_api_key.lua') then
     end)
   end
 
+  function openDarkSky()
+    hs.urlevent.openURLWithBundle('https://darksky.net/forecast/' .. latitude .. ',' .. longitude .. '/us12/en', hs.urlevent.getDefaultHandler('http'))
+  end
+
+  function getWeatherIcon(icon)
+    iconLabel = '☁️'
+    if icon == 'partly-cloudy-day' or icon == 'partly-cloudy-night' then
+      iconLabel = '⛅️'
+    elseif icon == 'snow' then
+      iconLabel = '❄️'
+    elseif icon == 'clear-day' then
+      iconLabel = '☀️'
+    elseif icon == 'clear-night' then
+      iconLabel = '🌙'
+    elseif icon == 'rain' or icon == 'sleet' then
+      iconLabel = '🌧'
+    elseif icon == 'fog' then
+      iconLabel = '🌫'
+    elseif icon == 'wind' then
+      iconLabel = '💨'
+    end
+    return iconLabel
+  end
+
   function updateWeather()
     status, data, headers = hs.http.get('https://api.darksky.net/forecast/' .. apiKey .. '/' .. latitude .. ',' .. longitude, {})
     if status == 200 then
       json = hs.json.decode(data)
-      temperature = math.floor(json.currently.temperature)
-      iconLabel = '☁️'
+      temperature = math.floor(json.currently.apparentTemperature)
       icon = json.currently.icon
-      if icon == 'partly-cloudy-day' or icon == 'partly-cloudy-night' then
-        iconLabel = '⛅️'
-      elseif icon == 'snow' then
-        iconLabel = '❄️'
-      elseif icon == 'clear-day' then
-        iconLabel = '☀️'
-      elseif icon == 'clear-night' then
-        iconLabel = '🌙'
-      elseif icon == 'rain' or icon == 'sleet' then
-        iconLabel = '🌧'
-      elseif icon == 'fog' then
-        iconLabel = '🌫'
-      elseif icon == 'wind' then
-        iconLabel = '💨'
+      menuIconLabel = getWeatherIcon(icon)
+      menuTable = {
+        {
+          title = 'darksky.net',
+          fn = function() openDarkSky() end
+        },
+        {
+          title = '-'
+        }
+      }
+      for i = 1, 8 do
+        table.insert(menuTable, {
+          title = os.date("%I%p", json.hourly.data[i].time):lower() .. "\t" .. getWeatherIcon(json.hourly.data[i].icon) .. "\t" .. math.floor(json.hourly.data[i].apparentTemperature) .. '°',
+          fn = function() openDarkSky() end
+        })
       end
-      menuWeather:setTitle(iconLabel .. ' ' .. temperature .. '°')
+      menuWeather:setTitle(menuIconLabel .. ' ' .. temperature .. '°')
+      menuWeather:setMenu(menuTable)
     end
   end
 
   local setMenu = function()
-    hs.urlevent.openURLWithBundle('https://darksky.net/forecast/' .. latitude .. ',' .. longitude .. '/us12/en', hs.urlevent.getDefaultHandler('http'))
+    openDarkSky()
   end
 
-  menuWeather:setClickCallback(setMenu)
   weatherTimerSet()
   updateWeather()
 
