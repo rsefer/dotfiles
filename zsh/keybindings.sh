31e1f7ee9e219382af02c3d567b7bb79e87cd065
Robert Sefer <rsefer@gmail.com>
2018-10-20 15:40:31 -0500

    This is a test

---
+++ zsh/keybindings.sh
@@ -0,0 +1,13 @@
+function git_prepare() {
+				if [ -n "$BUFFER" ];
+				then
+								BUFFER="git add -A && git commit -m \"$BUFFER\" && git push"
+				fi
+				if [ -z "$BUFFER" ];
+				then
+								BUFFER="git add -A && git commit -v && git push"
+				fi
+				zle accept-line
+}
+zle -N git_prepare
+bindkey "^g" git_prepare
