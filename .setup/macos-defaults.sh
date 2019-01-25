4ed14ed7f84702b388f2156d7fc2289d555cd73c
Robert Sefer <rsefer@gmail.com>
2019-01-24 20:43:39 -0600

    More defaults

--- .setup/macos-defaults.sh
+++ .setup/macos-defaults.sh
@@ -1,9 +1,13 @@
 # Dock
 defaults write com.apple.dock tilesize -int 48 # icon size
 defaults write com.apple.dock orientation -string left # position
-defaults write com.apple.dock autohide-time-modifier -float 0 # hide/show delay timing
-defaults write com.apple.dock autohide-delay -float 0 # auto-hiding delay timing
-
-defaults write com.apple.dashboard mcx-disabled -bool true # disable Dashboard
-
+defaults write com.apple.dock autohide-time-modifier -float 0 # no hide/show delay
+defaults write com.apple.dock autohide-delay -float 0 # no hide delay
 killall Dock
+
+# Finder
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv" # list view in Finder
+killall Finder
+
+# Misc.
+defaults write com.apple.dashboard mcx-disabled -bool true # disable Dashboard
