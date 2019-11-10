6ea8809981340d543d77677324d0c579faaa98e0
Robert Sefer <rsefer@roberts-imac.local>
2019-11-10 12:24:21 -0600

    Changed symlink to dot

--- setup/installs/dotfiles.sh
+++ setup/installs/dotfiles.sh
@@ -4,8 +4,8 @@ source $DOTFILES_ROOT/setup/functions.sh
 info 'installing dotfiles'
 
 overwrite_all=false backup_all=false skip_all=false
-for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+for src in $(find -H "$DOTFILES_ROOT/dots" -maxdepth 1 -name '.*' ! -name '.DS_Store' -not -path '..' -not -path '.' -not -path '*.git*')
 do
-	dst="$HOME/.$(basename "${src%.*}")"
+	dst="$HOME/$(basename "${src}")"
 	link_file "$src" "$dst"
 done
