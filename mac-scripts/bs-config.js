acf3dd4ddca0eddb8b331eb1641891c477686b1b
Robert Sefer <rsefer@gmail.com>
2020-05-10 11:39:47 -0500

    Browser sync improvements

---
+++ mac-scripts/bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	https: {
+		key: '/Users/rsefer/.localhost-ssl/server.key',
+		cert: '/Users/rsefer/.localhost-ssl/server.crt'
+	},
+	open: false,
+	server: true,
+	watch: true
+};
