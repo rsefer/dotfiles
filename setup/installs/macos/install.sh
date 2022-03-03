e510de633cbeba8dc012ac274cfdde2773853c30
Robert Sefer <rsefer@gmail.com>
2022-03-03 14:01:13 -0600

    Spark

--- setup/installs/macos/install.sh
+++ setup/installs/macos/install.sh
@@ -19,6 +19,7 @@ defaults write com.apple.sock show-recents -bool false # don't show recently use
 
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Fantastical.app"
+dockutil --no-restart --add "/Applications/Spark.app"
 dockutil --no-restart --add "/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/Home.app"
 dockutil --no-restart --add "/Applications/Home Assistant.app"
