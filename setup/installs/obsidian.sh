b076f17cc96724000b0149d3a05e088157486b32
Robert Sefer <rsefer@gmail.com>
2022-08-24 20:00:20 -0500

    Changed obsidian vault location

--- setup/installs/obsidian.sh
+++ setup/installs/obsidian.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source $DOTFILES_ROOT/setup/functions.sh
 
-rm -r ~/Library/Mobile\ Documents/com~apple~CloudDocs/Work/Notes/.obsidian
-ln -s $DOTFILES_ROOT/dots/.obsidian/ ~/Library/Mobile\ Documents/com~apple~CloudDocs/Work/Notes/.obsidian
+rm -r ~/Library/Mobile\ Documents/com~apple~CloudDocs/Notes/.obsidian
+ln -s $DOTFILES_ROOT/dots/.obsidian/ ~/Library/Mobile\ Documents/com~apple~CloudDocs/Notes/.obsidian
