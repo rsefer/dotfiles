dcd3be21539567c6ba3cf48652f7699e878a222b
Robert Sefer <rsefer@gmail.com>
2024-05-20 13:27:42 -0500

    Reset app windows via hammerspoon

---
+++ dots/.raycast/scripts/reset-app-windows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Reset App Windows (via Hammerspoon)
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🖥️
+
+# Documentation:
+# @raycast.author rsefer
+# @raycast.authorURL https://raycast.com/rsefer
+
+open hammerspoon://resetAllApps
