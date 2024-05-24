fd81b1ade572371a41e1e4711ddbefca13003f8b
Robert Sefer <rsefer@gmail.com>
2024-05-23 22:26:56 -0500

    Karabiner elements + hyper key

--- setup/installs/dotfiles.sh
+++ setup/installs/dotfiles.sh
@@ -4,9 +4,16 @@ source $DOTFILES_ROOT/setup/functions.sh
 info 'installing dotfiles'
 
 overwrite_all=false backup_all=false skip_all=false
-for src in $(find -H "$DOTFILES_ROOT/dots" -maxdepth 1 -name '.*' ! -name '.DS_Store' -not -path '..' -not -path '.' -not -path '*.git*')
+for src in $(find -H "$DOTFILES_ROOT/dots" -maxdepth 1 -name '.*' ! -name '.DS_Store' ! -name '.config' -not -path '..' -not -path '.' -not -path '*.git*')
 do
 	dst="$HOME/$(basename "${src}")"
+	link_file "$src" "$dst"
+done
+
+# .config is treated differently
+for src in $(find -H "$DOTFILES_ROOT/dots/.config" -maxdepth 1 ! -name '.DS_Store' ! -name '.config' -not -path '..' -not -path '.' -not -path '*.git*')
+do
+	dst="$HOME/.config/$(basename "${src}")"
 	link_file "$src" "$dst"
 done
 
