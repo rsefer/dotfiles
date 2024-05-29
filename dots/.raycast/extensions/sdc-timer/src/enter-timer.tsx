b9dec733e334bbd015110ae4a233a5a59cf019ad
Robert Sefer <rsefer@gmail.com>
2024-05-29 10:11:58 -0500

    Removed raycast

--- dots/.raycast/extensions/sdc-timer/src/enter-timer.tsx
+++
@@ -1,5 +0,0 @@
-import { launchCommand, LaunchType } from "@raycast/api";
-
-export default function Command() {
-	return launchCommand({ name: "choose-client", type: LaunchType.UserInitiated, context: { timerType: "enter" } });
-}
--- dots/.raycast/extensions/sdc-timer/src/toggle-timer.tsx
+++
@@ -1,9 +0,0 @@
-import { launchCommand, LaunchType } from "@raycast/api";
-import { runningTimer, stopTimer } from "./Timer";
-
-export default async function Command() {
-	if (await runningTimer()) {
-		return await stopTimer();
-	}
-	return launchCommand({ name: "choose-client", type: LaunchType.UserInitiated, context: { timerType: "running" } });
-}
