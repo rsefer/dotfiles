102239b5827b1cadc3f7dc8072195b05ebe4726f
Robert Sefer <rsefer@gmail.com>
2025-07-01 11:46:20 -0500

    NVM refactor

--- zsh/nvmrc-autoswitch.sh
+++ zsh/nvmrc-autoswitch.sh
@@ -1,14 +1,17 @@
 autoload -U add-zsh-hook
 load-nvmrc() {
-	local node_version="$(nvm version)"
-	local nvmrc_path="$(nvm_find_nvmrc)"
-
+	local node_version nvmrc_path nvmrc_node_version
+	node_version="$(nvm version)"
+	nvmrc_path="$(nvm_find_nvmrc)"
 	if [ -n "$nvmrc_path" ]; then
-		local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-		if [ "$nvmrc_node_version" = "N/A" ]; then
-			nvm install
-		elif [ "$nvmrc_node_version" != "$node_version" ]; then
-			nvm use
+		nvmrc_node_version="$(cat "${nvmrc_path}")"
+		if [ -n "$nvmrc_node_version" ]; then
+			nvmrc_node_version=$(nvm version "$nvmrc_node_version")
+			if [ "$nvmrc_node_version" = "N/A" ]; then
+				nvm install
+			elif [ "$nvmrc_node_version" != "$node_version" ]; then
+				nvm use
+			fi
 		fi
 	elif [ "$node_version" != "$(nvm version default)" ]; then
 		echo "Reverting to nvm default version"
